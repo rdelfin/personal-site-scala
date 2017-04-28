f2704929dd8d26185aab743e46834acf59cb315c
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-04-27 19:14:03 -0500

    Added template

--- src/main/scala/com/rdelfin/personalsite/PersonalSiteStack.scala
+++ src/main/scala/com/rdelfin/personalsite/PersonalSiteStack.scala
@@ -2,10 +2,6 @@ package com.rdelfin.personalsite
 
 import org.scalatra._
 import scalate.ScalateSupport
-import org.fusesource.scalate.{ TemplateEngine, Binding }
-import org.fusesource.scalate.layout.DefaultLayoutStrategy
-import javax.servlet.http.HttpServletRequest
-import collection.mutable
 
 trait PersonalSiteStack extends ScalatraServlet with ScalateSupport {
 
