197075e68f5c95300aff418e9ae91f919cae671a
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-04-27 23:41:03 -0500

    Fixed up projects page

--- src/main/scala/com/rdelfin/personalsite/MyScalatraServlet.scala
+++ src/main/scala/com/rdelfin/personalsite/MyScalatraServlet.scala
@@ -3,6 +3,13 @@ package com.rdelfin.personalsite
 import org.scalatra._
 
 class MyScalatraServlet extends PersonalSiteStack {
+  get("/projects") {
+    findTemplate("projects.ssp") map { path =>
+      contentType = "text/html"
+      layoutTemplate(path)
+    } get
+  }
+
   get("/projects/twitcher") {
     findTemplate("projects/twitcher.ssp") map { path =>
       contentType = "text/html"
